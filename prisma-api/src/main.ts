93aee3f968dd6e34a81f26148ce21a13eebfbd5c
Imane Sghiouar <isghioua@e1r8p8.1337.ma>
2022-11-19 17:15:25 +0100

    first

---
+++ prisma-api/src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, {
+    cors : {
+      origin : '*',
+      credentials : true
+    }
+  });
+  // app.enableCors({     origin: "*",     credentials: true   });
+
+  app.use(cookieParser());
+  await app.listen(5000);
+}
+bootstrap();
